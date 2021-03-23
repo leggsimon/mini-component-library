1a7b41168002da60a3672f894c9ea81b1d7022d6
Simon Legg <leggsimon@gmail.com>
2021-03-23 17:01:57 +0100

    create small progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,7 +5,32 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+	return (
+		<Wrapper
+			role="progressbar"
+			aria-valuenow={value}
+			aria-valuemin="0"
+			aria-valuemax="100"
+			aria-valuetext={`${value}% Completed`}
+		>
+			<Bar value={value} />
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	height: 8px;
+	border-radius: 4px;
+	width: 370px;
+	background-color: ${COLORS.transparentGray15};
+	box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+	overflow: hidden;
+`;
+
+const Bar = styled.div`
+	width: ${(p) => p.value + '%'};
+	height: 100%;
+	background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
